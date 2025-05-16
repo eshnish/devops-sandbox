d03f7ff80146b4d4ff0aa4d767b21964c68e4f15
Tarun Asthana <tarunasthana@IN-FVFGK2EPQ05N.local>
2025-05-17 00:16:15 +0530

    create conflict script

---
+++ git-commands/create_conflict.sh
@@ -0,0 +1,15 @@
+#Create new branch
+git checkout -b feature
+
+#Modify README.md on feature branch
+echo "Feature File" > README.md
+
+git add README.md
+git commit -m "Changes from feature branch"
+
+git checkout master
+echo "Edited README.md content" > README.md
+git add README.md
+git commit -m "New Changes in Master"
+
+git merge feature-branch
